3cb9058df10a3516f3dba2afe54e5487dcb79346
Jason Liang <jason.liang2012@gmail.com>
2015-11-20 05:41:36 +0000

    Added plan variables and params to homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  belongs_to :plan
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  belongs_to :plan
 end
